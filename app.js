5e928aa9ec620e52b3a44f10717710dfb37bd881
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-07 15:43:43 -0800

    added renderPokemon and linked to test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
+import { getPokedex } from "./fetch.js";
 
 // let state
-
 // set event listeners 
   // get user input
   // use user input to update state 
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,18 +1,20 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderPokemon } from '../utils.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('render a pokemon', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<div><h4>butterfree</h4><img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png"></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderPokemon();
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
