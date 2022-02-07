e1705a175ec374d91f9a32a1c39fae08442dec59
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-07 15:58:12 -0800

    passing render pokemon function

--- test/utils.test.js
+++ test/utils.test.js
@@ -8,12 +8,15 @@ const test = QUnit.test;
 test('render a pokemon', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<div><h4>butterfree</h4><img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png"></div>';
+    const expected = '<a href="./pokemon/?pokemon=butterfree"><div><h4>butterfree</h4><img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png"></div></a>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderPokemon();
+    const actual = renderPokemon({
+        pokemon: 'butterfree',
+        url_image: 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',
 
+    });
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
--- utils.js
+++ utils.js
@@ -1,3 +1,13 @@
 export function renderPokemon(pokemon){
-    
+    const a = document.createElement('a');
+    const div = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const img = document.createElement('img');
+
+    h4.textContent = pokemon.pokemon;
+    img.src = pokemon.url_image;
+    a.href = `./pokemon/?pokemon=${pokemon.pokemon}`;
+    div.append(h4, img);
+    a.append(div);
+    return a;
 }
