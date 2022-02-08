a3356377d40c1efad08ca2750bf7d2ec91fe919e
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-07 17:50:37 -0800

    when link is clicked more pokemon information is displayed.

---
+++ pokemon/pokemon.js
@@ -0,0 +1,18 @@
+import { findById, makeCard } from "../utils.js";
+import { getPokedex } from "../fetch.js";
+
+const pokemonEl = document.getElementById('pokemon-card');
+
+const params = new URLSearchParams(window.location.search);
+
+async function renderCard() {
+    const data = await getPokedex();
+    
+    const pokemon = await findById(params.get('pokemon'), data);
+    console.log('obj', pokemon);
+    const card = makeCard(pokemon);
+
+    pokemonEl.append(card);
+
+}
+renderCard();
--- utils.js
+++ utils.js
@@ -1,3 +1,27 @@
+export function makeCard(pokemon) {
+    const div = document.createElement('div');
+    const name = document.createElement('h3');
+    const hp = document.createElement('p');
+    const attack = document.createElement('p');
+    const img = document.createElement('img');
+    const defense = document.createElement('p');
+    const type = document.createElement('p');
+    const a = document.createElement('a');
+
+    name.textContent = pokemon.pokemon;
+    hp.textContent = pokemon.hp;
+    attack.textContent = pokemon.attack;
+    defense.textContent = pokemon.defense;
+    type.textContent = pokemon.type_1;
+    img.src = pokemon.url_image;
+    a.href = pokemon.pokedex;
+
+    div.append(name, hp, attack, defense, type, img);
+    a.append(div);
+
+    return a;
+}
+
 export function renderPokemon(pokemon){
     const a = document.createElement('a');
     const div = document.createElement('div');
@@ -12,6 +36,6 @@ export function renderPokemon(pokemon){
     return a;
 }
 
-export function findById(id, data) {
+export async function findById(id, data) {
     return data.find((item) => item.pokemon === id);
 }
