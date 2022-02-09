f4383f7892988ee363bb61d840bdf4f0b922efff
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-08 16:12:27 -0800

    added styling

--- utils.js
+++ utils.js
@@ -1,18 +1,24 @@
 export function makeCard(pokemon) {
     const div = document.createElement('div');
+    div.classList.add('card');
     const name = document.createElement('h3');
+    div.classList.add('card-name');
     const hp = document.createElement('p');
+    hp.classList.add('hp');
     const attack = document.createElement('p');
+    attack.classList.add('attack');
     const img = document.createElement('img');
     const defense = document.createElement('p');
+    defense.classList.add('defense');
     const type = document.createElement('p');
+    type.classList.add('type');
     const a = document.createElement('a');
 
-    name.textContent = pokemon.pokemon;
-    hp.textContent = pokemon.hp;
-    attack.textContent = pokemon.attack;
-    defense.textContent = pokemon.defense;
-    type.textContent = pokemon.type_1;
+    name.textContent = pokemon.pokemon.toUpperCase();
+    hp.textContent = 'HP: ' + pokemon.hp;
+    attack.textContent = 'ATTACK: ' + pokemon.attack;
+    defense.textContent = 'DEFENSE: ' + pokemon.defense;
+    type.textContent = 'TYPE: ' + pokemon.type_1.toUpperCase();
     img.src = pokemon.url_image;
     a.href = pokemon.pokedex;
 
@@ -25,10 +31,12 @@ export function makeCard(pokemon) {
 export function renderPokemon(pokemon){
     const a = document.createElement('a');
     const div = document.createElement('div');
+    div.classList.add('select-card');
     const h4 = document.createElement('h4');
+    h4.classList.add('select-name');
     const img = document.createElement('img');
 
-    h4.textContent = pokemon.pokemon;
+    h4.textContent = pokemon.pokemon.toUpperCase();
     img.src = pokemon.url_image;
     a.href = `./pokemon/?pokemon=${pokemon.pokemon}`;
     div.append(h4, img);
